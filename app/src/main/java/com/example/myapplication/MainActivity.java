8ce2a9972275595e9686939c0103fc68a731890c
KirillColeBou <kirillteplycov888@gmail.com>
2024-06-01 11:56:14 +0500

    Объявление переменных. Тепляков

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaRecorder mediaRecorder;
+    private MediaPlayer mediaPlayer;
+    private String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
